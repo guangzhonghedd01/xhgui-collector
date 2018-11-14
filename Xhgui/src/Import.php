f4870f01729f542e78b41a02d9bc110c637d7d1b
Gilbert.He <guangzhonghe@hk01.com>
2018-11-14 16:38:43 +0800

    修复文件名带.的bug

--- Xhgui/src/Import.php
+++ Xhgui/src/Import.php
@@ -89,6 +89,15 @@ class Import
                     $data['profile'] = xhprof_disable();
                 }
 
+                if (!empty($data['profile'])){
+                    $profile = [];
+                    foreach($data['profile'] as $key => $value) {
+                        $profile[strtr($key, ['.' => '_'])] = $value;
+                    }
+
+                    $data['profile'] = $profile;
+                }
+
                 // ignore_user_abort(true) allows your PHP script to continue executing, even if the user has terminated their request.
                 // Further Reading: http://blog.preinheimer.com/index.php?/archives/248-When-does-a-user-abort.html
                 // flush() asks PHP to send any data remaining in the output buffers. This is normally done when the script completes, but
